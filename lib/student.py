c9d798f8cb821852d2bdc4971a57dff268f75110
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-24 14:36:34 +0300

    Complete asignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        # Call the constructor of the parent class
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, teaching):
+        # Add the provided teaching to the student's knowledge list
+        self.knowledge.append(teaching)
